61c9c80abea9d67da36becfa81c0252108c21d74
dopez <devops@daum.net>
2015-05-26 01:50:43 +0900

    [Sample] Ajax호출에 대한 MsgManager를 이용한 Global 에러처리

--- java/com/ubi/erp/user/controller/TestController.java
+++ java/com/ubi/erp/user/controller/TestController.java
@@ -3,6 +3,7 @@ package com.ubi.erp.user.controller;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
@@ -13,12 +14,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.ubi.erp.cmm.util.gson.JsonUtil;
 import com.ubi.erp.user.domain.Test;
 import com.ubi.erp.user.service.TestService;
 
@@ -59,18 +62,40 @@ public class TestController {
 	@RequestMapping(value = "/prcs", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.OK)
 	public void prcsTest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String jsonData = request.getParameter("jsonData");
-		List<Test> list = new ArrayList<Test>();
-		ObjectMapper mapper = new ObjectMapper();
-		list = mapper.readValue(jsonData, new TypeReference<ArrayList<Test>>(){});
-		
-		for(Test test : list) {
-			if ("CREATE".equals(test.getCudKey())) {
-				testService.insTest(test);
-			} else if ("UPDATE".equals(test.getCudKey())) {
-				testService.updTest(test);
-			} else if ("DELETE".equals(test.getCudKey())) {
-				testService.delTest(test);
+		Hashtable<String, String> ht = new Hashtable<String, String>();
+
+		try {
+			String jsonData = request.getParameter("jsonData");
+			List<Test> list = new ArrayList<Test>();
+			ObjectMapper mapper = new ObjectMapper();
+			list = mapper.readValue(jsonData, new TypeReference<ArrayList<Test>>() {
+			});
+
+			for (Test test : list) {
+				if ("CREATE".equals(test.getCudKey())) {
+					testService.insTest(test);
+				} else if ("UPDATE".equals(test.getCudKey())) {
+					testService.updTest(test);
+				} else if ("DELETE".equals(test.getCudKey())) {
+					testService.delTest(test);
+				}
+			}
+
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("rtnCode", "1");
+			String jsonStr = new String(JsonUtil.parseToString(map));
+			makeResponse(response, "json", jsonStr);
+
+		} catch (DuplicateKeyException e) {
+			ht.put("rtnCode", "-1");
+			ht.put("EXCEPTION_TYPE", "BIZ");
+			ht.put("EXCEPTION_MSG_CODE", "ERR005"); // PK 중복
+			//ht.put("EXCEPTION_MSG_PARAM", "");
+
+		} finally {
+			if (!ht.isEmpty()) {
+				response.setHeader("EXCEPTION", "Y");
+				makeResponse(response, "json", JsonUtil.parseToString(ht));
 			}
 		}
 	}
