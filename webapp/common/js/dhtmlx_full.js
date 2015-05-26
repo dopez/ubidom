abc19ffcab8c6c347ab68ab0dbca8e9bbf416304
ubiHoon <jhkim@ubidom.com>
2015-05-26 11:37:13 +0900

    dhx error extend js dhx common js 주석처리

--- webapp/common/js/dhtmlx_full.js
+++ webapp/common/js/dhtmlx_full.js
@@ -8,12 +8,12 @@ if(typeof dhtmlx == 'undefined' || dhtmlx == null)
 	document.write("<script type='text/javascript' src='/component/dhtmlxGrid/ext/dhtmlxgrid_srnd.js'></script>");
 	document.write("<script type='text/javascript' src='/component/dhtmlxGrid/ext/dhtmlxgrid_export.js'></script>");
 
-	document.write("<script type='text/javascript' src='/component/dxerror_extends.js'></script>");
+/*	document.write("<script type='text/javascript' src='/component/dxerror_extends.js'></script>");*/
 	document.write("<script type='text/javascript' src='/component/dhtmlx_extend.js'></script>");
 	document.write("<script type='text/javascript' src='/component/dxgrid_extend.js'></script>");
 	document.write("<script type='text/javascript' src='/component/dxtree_extend.js'></script>");
 	
-	document.write("<script type='text/javascript' src='/component/dhtmlx_common.js'></script>");
+	/*document.write("<script type='text/javascript' src='/component/dhtmlx_common.js'></script>");*/
 	//grid 및 form 조회화면 추가
 	
 	//dhtmlx - calendar 부분 추가 입력
