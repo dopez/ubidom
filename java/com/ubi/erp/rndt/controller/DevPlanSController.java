d1f87f36d5d32117331e1d8215747e98a3165541
ubiHoon <jhkim@ubidom.com>
2015-06-11 11:30:12 +0900

    개발계획등록

---
+++ java/com/ubi/erp/rndt/controller/DevPlanSController.java
@@ -0,0 +1,46 @@
+package com.ubi.erp.rndt.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ubi.erp.rndt.domain.DevPlanS;
+import com.ubi.erp.rndt.service.DevPlanSService;
+
+@RestController
+@RequestMapping(value = "/erp/rndt/stan/DevPlanS")
+public class DevPlanSController {
+
+	@Autowired
+	private DevPlanSService DevPlanSService;
+
+	private static final Logger logger = LoggerFactory.getLogger(DevPlanSController.class);
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = "/selFrmMain", method = RequestMethod.POST)
+	public List<DevPlanS> selFrmMain(HttpServletRequest request, HttpServletResponse response, HttpSession session, DevPlanS DevPlanS) throws Exception {
+		String comp = (String) session.getAttribute("compId");
+		String setDate = request.getParameter("setDate");
+		String setSeq = request.getParameter("setSeq");
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("V_COMPID", comp);
+		map.put("V_SET_DATE", setDate);
+		map.put("V_SET_SEQ", setSeq);
+		map.put("o_cursor", null);
+		DevPlanSService.selFrmMain(map);
+		List<DevPlanS> list = (List<DevPlanS>) map.get("o_cursor");
+		return list;
+	}
+}
---
+++ java/com/ubi/erp/rndt/service/DevPlanSService.java
@@ -0,0 +1,25 @@
+package com.ubi.erp.rndt.service;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ubi.erp.rndt.dao.DevPlanSDao;
+
+@Service
+public class DevPlanSService {
+	private DevPlanSDao dao;
+
+	@Autowired
+	public void setSqlSession(SqlSession sqlSession) {
+		dao = sqlSession.getMapper(DevPlanSDao.class);
+	}
+
+	public void selFrmMain(Map<String, Object> map) {
+		dao.selFrmMain(map);
+		
+	}
+
+}
