303334da65f9870e1bb7855614c834b6b874a2b8
pih6585 <pih6585@naver.com>
2015-06-23 09:55:33 +0900

    머지

--- webapp/component/component-extends/dhtmlx/dhtmlx_extend.js
+++ webapp/component/component-extends/dhtmlx/dhtmlx_extend.js
@@ -326,6 +326,7 @@ function fileupload(tagId,imgId,callbackFn){
 function searchDate(dateValue){
 	var value = null;
 	var splitfrDate = dateValue.split("/");
+	console.log(splitfrDate.length)
 	for(var i=0;i<splitfrDate.length;i++){
 		value = value + splitfrDate[i];
 	}
